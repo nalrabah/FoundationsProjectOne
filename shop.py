98102fafe216688f699b66b9a1eeec4dc339abc6
nalrabah <nalrabah@hotmail.com>
2019-01-03 14:15:48 +0300

    done

--- shop.py
+++ shop.py
@@ -24,7 +24,7 @@ def print_menu():
     print "There are " + str(len(menu)) + " items on the menu:"
     for key in menu:
       print "- %s: %s KD " % (key, menu[key]) 
-print_menu() ## How do I put coffee and tea next to each other?
+# print_menu() ## How do I put coffee and tea next to each other?
 
 
 def print_originals():
@@ -34,8 +34,8 @@ def print_originals():
     print("Our original flavor cupcakes (KD %s each):" % original_price)
     # your code goes here!
     for cupcakes in original_flavors:
-      print cupcakes
-print_originals()
+      print "- %s" % cupcakes
+# print_originals()
 
 def print_signatures():
     """
@@ -44,16 +44,14 @@ def print_signatures():
     print("Our signature flavor cupcake (KD %s each):" % signature_price)
     # your code goes here!
     for signature in signature_flavors:
-        print signature
+        print  "- %s" % signature
 
 def is_valid_order(order):
     """
     Check if an order exists in the shop.
     """
-    if order in original_flavors or signature_flavors: 
+    if (order in original_flavors) or (order in signature_flavors) or (order in menu): 
       return True
-    elif order = "Exit"
-      break
     else:
       return False
 
@@ -63,13 +61,19 @@ def get_order():
     """
     order_list = []
     # your code goes here!
-    order= raw_input("What would you like to order? (Enter the exact spelling of the item you would like to order. Type 'Exit' to end your order."))
-    while is_valid_order(order) = True:
-      order_list.append(order)
-    Else:
-      
-    
+    order= raw_input("What would you like to order? (Enter the exact spelling of the item you would like to order. Type 'Exit' to end your order. \n")
+    while order.lower() != "exit":
+        if is_valid_order(order) == True:
+            order_list.append(order)
+            order = raw_input("What else? \n")
+        elif is_valid_order(order) == False:
+            order = raw_input("Please enter a valid order \n")
+      # if order == "Exit":
+      #   print("I am now breaking")
+      #   break
+      # order_list.append(order)
 
+    print(order_list)
     return order_list
 
 
@@ -77,14 +81,23 @@ def accept_credit_card(total):
     """
     Return whether an order is eligible for credit card payment.
     """
-    print "Your order is eligible for credit card payment"
-
+    if price > 4:
+      print "Your order is eligible for credit card payment"
+    else:
+      print "Sorry! Your order is not eligible for credit card payment"
 
 def get_total_price(order_list):
     """
     Calculate and return total price of the order.
     """
     total = 0
+    for order in order_list:
+        if order in original_flavors:
+            total+= original_price
+        elif order in signature_flavors:
+            total+= signature_price
+        else:
+            total+= menu[order]
     # your code goes here!
 
     return total
@@ -97,3 +110,6 @@ def print_order(order_list):
     print()
     print("Your order is: ")
     # your code goes here!
+    print(order_list)
+    total = get_total_price(order_list)
+    print "Your total is %s KD. Thank you for shopping at Sugar Rush. Come back soon!" % total 
